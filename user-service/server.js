f928283643b9452975e59a1a6511a6208e0fe0fb
saikiran601 <bommidenisaikiran423@gmail.com>
2025-04-05 18:51:52 +0530

    add cors

--- user-service/server.js
+++ user-service/server.js
@@ -12,11 +12,11 @@ app.use(express.json());
 
 app.use(cors())
 
-// app.use(cors({
-//   origin: ['https://simpleapp-i551.onrender.com'], // Add your frontend URL
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: ['https://texus.onrender.com'], // Add your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 
 // const allowedOrigins = [
